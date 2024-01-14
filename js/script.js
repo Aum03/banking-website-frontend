48b4ede0b434f68700b471e439fb9146f97f0b60
Aum <aumyadav0311@gmail.com>
2024-01-14 20:53:59 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const navLinks=document.querySelector(".nav-links");
+const hamburgerBtn=document.querySelector(".hamburger-btn");
+const toggleImg=document.querySelector("#toggle-id");
+
+hamburgerBtn.addEventListener('click',()=>{
+    navLinks.classList.toggle('open');
+  isOpen=navLinks.classList.contains('open');
+
+  if(isOpen){
+
+    toggleImg.src='./images/icon-close.svg'
+
+  }
+  else{
+      toggleImg.src='./images/icon-hamburger.svg'
+  }
+
+})
